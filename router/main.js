d55da83a06dcc66c0da67cf751308e0df834e611
변형준 <qus1225@gmail.com>
2017-07-15 20:07:35 +0900

    Add detail sample

--- router/main.js
+++ router/main.js
@@ -3,4 +3,7 @@ module.exports = function(app)
   app.get('/',function(req,res){
     res.render('index')
   });
+  app.get('/rooms/flower',function(req,res){
+    res.render('rooms/flower')
+  });
 }
