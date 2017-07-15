847a24931e40d4af096853ad53ed2d6adf5b698c
변형준 <qus1225@gmail.com>
2017-07-15 14:35:56 +0900

    Set KeyVisual

--- public/js/template.js
+++ public/js/template.js
@@ -14,17 +14,7 @@ jQuery(document).ready(function($) {
 		} else {
 			my_nav.removeClass( 'stick' );
 		}   
-	};
-
-	var initio_parallax_animation = function() { 
-		$('.parallax').each( function(i, obj) {
-			var speed = $(this).attr('parallax-speed');
-			if( speed ) {
-				var background_pos = '-' + (window.pageYOffset / speed) + "px";
-				$(this).css( 'background-position', 'center ' + background_pos );
-			}
-		});
-	}
+	};gi
 	
 	// run our function on load
 	sticky_navigation();
